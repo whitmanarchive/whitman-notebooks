33676b4f1709caaf34685acb13dde3b2a683f1f9
Karin Dalziel <kdalziel2@unl.edu>
2023-11-28 13:44:32 -0600

    linked shared methods file

--- scripts/overrides/tei_to_es.rb
+++ scripts/overrides/tei_to_es.rb
@@ -1,4 +1,6 @@
 require_relative "../../../whitman-scripts/scripts/ruby/get_works_info.rb"
+require_relative "../../../whitman-scripts/scripts/ruby/get_works_info.rb"
+
 class TeiToEs
 
   ################
